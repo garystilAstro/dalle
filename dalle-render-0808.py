283f61bbee78b90a136b90b069c6d75cb4900f2a
garystilAstro <161110593+garystilAstro@users.noreply.github.com>
2024-09-09 15:07:29 -0600

    Update dalle-render-0808.py

--- dalle-render-0808.py
+++ dalle-render-0808.py
@@ -15,7 +15,7 @@ client = OpenAI(api_key=api_key)
 
 @app.route('/')
 def index():
-    return render_template('dalle-local-menu-css-0720.html')
+    return render_template('dalle-render-0808.html.html')
 
 @app.route('/generate_image', methods=['POST'])
 def generate_image():
